13862472820b01312b6a8ad83435364ae1639cbd
unknown <wenhuzha@CNU418C50Z.nsn-intra.net>
2017-04-20 17:54:57 +0800

    function node requirement document format update

--- 象山县党员/象山县党员/websrc/govauditsys-sever/govauditsys/src/main/java/com/ss/govauditsys/sysdata/model/CommunistInfoRespository.java
+++ 象山县党员/象山县党员/websrc/govauditsys-sever/govauditsys/src/main/java/com/ss/govauditsys/sysdata/model/CommunistInfoRespository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-public interface CommunistInfoRespository extends PagingAndSortingRepository<CommunistInfo, Long> {
+public interface CommunistInfoRespository extends PagingAndSortingRepository<CommunistInfo, Long>, QueryDslPredicateExecutor<CommunistInfo> {
 	@Override
 	CommunistInfo save(CommunistInfo communistInfo);
 
